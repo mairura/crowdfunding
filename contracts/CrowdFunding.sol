a7ad46e2aad48ea0d8f899cf8e55015873d8496f
ted <mairurat@gmail.com>
2024-01-04 23:05:27 +0300

    first commit

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
-}
+     struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+         uint256 amountCollected;
+         string image;
+         address[] donators;
+         uint256[] donations;
+     }
+
+     mapping(uint256 => Campaign) campaigns;
+
+     uint256 public numberOfCampaigns = 0;
+} 
